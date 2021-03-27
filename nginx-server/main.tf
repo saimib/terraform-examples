56c20d04916298c40da0d3901d9dc19d8ddcb35a
saimib <manibalan23@hotmail.com>
2021-03-27 18:19:05 +0530

    Add comments in resource file in nginx-server project
    added comments that will be helpful for this beginner project. People with zero knowledge on terraform may find this helpful to learn hcl for the first time

--- nginx-server/main.tf
+++ nginx-server/main.tf
@@ -1,15 +1,35 @@
+/*  
+    Terraform block 
+    This is required to know which provider to download from Terraform Registry
+    For more, check https://learn.hashicorp.com/tutorials/terraform/aws-build?in=terraform/aws-get-started#terraform-block 
+*/
 terraform {
   required_providers {
     docker = {
-      source = "kreuzwerker/docker"
+      source = "kreuzwerker/docker" // Shorthand for registry.terraform.io/kreuzwerker/docker, more providers can be found in https://registry.terraform.io/browse/providers
     }
   }
 }
 
+/*  
+    Provider block 
+    This configures the named provider, in our case aws. Multiple provider blocks can exist to manage resources from different providers.
+    For more, check https://learn.hashicorp.com/tutorials/terraform/aws-build?in=terraform/aws-get-started#providers
+*/
 provider "docker" {
-
+  /* 
+    Configuration options
+    Credentials are taken from environment variables when profile is not specified
+    For more, check https://registry.terraform.io/providers/hashicorp/aws/latest/docs#authentication 
+  */
 }
 
+
+/* 
+    Resources block
+    This block defines a piece of infrastructure. This can be physical like ec2 or logical like a REST API
+    For more, check https://learn.hashicorp.com/tutorials/terraform/aws-build?in=terraform/aws-get-started#resources 
+*/
 resource "docker_image" "nginx" {
   name         = "nginx:latest"
   keep_locally = false
