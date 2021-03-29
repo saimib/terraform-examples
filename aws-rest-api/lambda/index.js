1a9eb4ade0a9b7c1a14e971fdc44ec7ab79aecb6
saimib <manibalan23@hotmail.com>
2021-03-29 08:44:18 +0530

    Add mock response from lambda
    added a mock response in the common lambda handler to return a mock blog post response for all API requests

--- aws-rest-api/lambda/index.js
+++ aws-rest-api/lambda/index.js
@@ -1,4 +1,23 @@
-exports.handler =  async function(event, context) {
-    console.log(event.requestContext.path)
-    return event.requestContext.path
+exports.handler =  function(event, context, callback) {
+    
+    let res = {
+        status: 200,
+        body: [
+            {
+              "id": "d290f1ee-6c54-4b01-90e6-d701748f0851",
+              "author": "George Washington",
+              "date": "2016-08-29T09:12:33.001Z",
+              "content": "sample content",
+              "comments": [
+                {
+                  "id": "d290f1ee-6c54-4b01-90e6-d701748f0855",
+                  "author": "Raul nevari",
+                  "date": "2016-08-29T09:12:33.001Z",
+                  "content": "sample comment"
+                }
+              ]
+            }
+        ]
+    }
+    callback(null, res)
 }
